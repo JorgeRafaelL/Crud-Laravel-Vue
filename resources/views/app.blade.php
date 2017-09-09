a3133e8b117a7dff9c66612ffd2ab31bcc707e61
JorgeRafaelL <jorge.alejo.rafael@gmail.com>
2017-09-08 22:51:41 -0500

    Añade un tema para el sitio

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,17 +8,18 @@
         <title>Laravel & VUE</title>
 
        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        
+       <link rel="stylesheet" href="https://bootswatch.com/slate/bootstrap.min.css">
+
     </head>
     <body>
 
     <div class="container">
-       
+
         @yield('content')
 
     </div>
 
     <script src="{{ asset('js/app.js') }}"></script>
-        
+
     </body>
 </html>
