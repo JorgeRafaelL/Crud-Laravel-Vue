9f3197ea60fc4423b202f1184c5a18490e16fd74
JorgeRafaelL <jorge.alejo.rafael@gmail.com>
2017-09-08 22:52:58 -0500

    Añade un formulario para una nueva tarea

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,3 +1,4 @@
+<form v-on:submit.prevent="createKeep" method="post">
 <div class="modal fade" id="create">
 	<div class="modal-dialog">
 		<div class="modal-content">
@@ -8,14 +9,17 @@
 					<h4>Nueva tarea</h4>
 			</div>
 			<div class="modal-body">
-				....
+				<label for="keep">Crear tarea</label>
+				<input type="text" name="keep" class="form-control" v-model="newKeep">
+				<span v-for="error in errors" class="text-danger">@{{ error }}</span>
 			</div>
 			<div class="modal-footer">
 				<input type="submit" class="btn btn-primary" value="Guardar">
 			</div>
-	
+
 		</div>
-	
+
 	</div>
-	
+
 </div>
+</form>
